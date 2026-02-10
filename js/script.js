1624045f0b1eb2b119ba52d357816d5b97dceeab
Subaranjani R <subaranjani0304@gmail.com>
2026-02-10 18:17:25 +0530

    UrbanBags Project completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let barNav = document.querySelector(".bar-nav");
+
+  function btnClick(){
+    if (barNav.style.display === "block") {
+      barNav.style.display = "none";
+    } else {
+      barNav.style.display = "block";
+    }
+}
